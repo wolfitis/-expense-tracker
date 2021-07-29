08d0390ea5c3fea5499f12b3b6d349e47966ad91
wolfitis <webblog123@gmail.com>
2021-07-29 09:07:48 -0400

    add transaction to TransactionList

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    // Creating a new transaction
+    const newTransaction = {
+      // not the best way to generate unique ids
+      id: Math.floor(Math.random() * 100000),
+      text,
+      // to parse into number, otherwise it'll give error
+      amount: +amount
+    }
+    // Adding newly created transaction to our list
+    addTransaction(newTransaction);
+  }
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
